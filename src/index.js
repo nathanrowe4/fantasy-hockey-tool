7e6fc73075eaa6336f49e02e3ce925c71a68ba57
nathanrowe4 <nathanrowe@bell.net>
2019-10-06 14:27:06 -0400

    Add player express router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 require('./db/mongoose');
+const playerRouter = require('./routers/player')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(playerRouter)
 
 app.listen(port, () => {
   console.log("Server is up on port ", port)
---
+++ src/routers/player.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const Player = require('../models/player')
+
+const router = express.Router()
+
+// PLAYER ROUTES
+
+module.exports = router
