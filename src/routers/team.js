3e8bccb3aca980660ffdf9a39e591745cef6a9b3
nathanrowe4 <nathanrowe@bell.net>
2019-10-13 21:13:58 -0400

    Add route to create team and add player to team

---
+++ src/routers/team.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Team = require('../models/team')
+
+const router = express.Router()
+
+router.post('/team', async (req, res) => {
+  const team = new Team(req.body)
+
+  try {
+    await team.save()
+    res.status(201).send(team)
+  } catch(error) {
+    res.status(500).send(error)
+  }
+})
+
+router.get('/team/addPlayer', async (req, res) => {
+  try {
+    const teamId = mongoose.Types.ObjectId(req.body.teamId)
+    const newPlayerId = mongoose.Types.ObjectId(req.body.playerId)
+
+    const team = await Team.findOneAndUpdate({ _id: teamId },
+      {$push: { Players: newPlayerId }}, {useFindAndModify: false})
+    if(!team) {
+      throw new Error()
+    }
+
+    res.send(team)
+  } catch(error) {
+    res.status(404).send(error)
+  }
+})
+
+module.exports = router
