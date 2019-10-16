1a32b02e89a916b30d6c07b8d3089fe640cab58b
nathanrowe4 <nathanrowe@bell.net>
2019-10-15 22:01:10 -0400

    Complete updating documentation for player and team routes

--- src/routers/player.js
+++ src/routers/player.js
@@ -3,7 +3,7 @@ const Player = require('../models/player')
 
 const router = express.Router()
 
-// GET player by id
+// GET: Get player by id
 router.get('/players/:id', async (req, res) => {
   try {
     const player = await Player.findById(req.params.id)
@@ -18,7 +18,7 @@ router.get('/players/:id', async (req, res) => {
   }
 })
 
-// GET player by any parameter
+// GET: Get player by parameter in body
 router.get('/players', async (req, res) => {
   try {
     const player = await Player.findOne( req.body )
