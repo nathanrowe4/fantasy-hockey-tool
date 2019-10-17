b277d2199f00cf19f5f1e6f332c0b0a348fad3bb
nathanrowe4 <nathanrowe@bell.net>
2019-10-16 23:00:17 -0400

    Add route to find 2 players by body and compare their stats

--- src/routers/player.js
+++ src/routers/player.js
@@ -16,6 +16,19 @@ async function getPlayerFromDatabase(query) {
   return player
 }
 
+// Helper function to calculate difference of 2 players stats
+function getDifference(players) {
+  const categories = ['G', 'A', 'PIM', 'PLUSMINUS', 'PIM', 'SOG', 'PPP', 'HITS', 'FOW']
+  var differenceObj = {}
+
+  for(var count = 0; count < categories.length; count++) {
+    var category = categories[count]
+    differenceObj[category] = players[0][category] - players[1][category]
+  }
+
+  return differenceObj
+}
+
 // GET: Get player by id
 router.get('/players/:id', async (req, res) => {
   try {
@@ -43,6 +56,34 @@ router.get('/players', async (req, res) => {
     res.send(player)
   } catch(error) {
     res.status(404).send()
+  }
+})
+
+// GET: Compare players by parameter in body
+router.get('/comparePlayers', async (req, res) => {
+  try {
+    const keys = Object.keys(req.body)
+
+    // iterate through body arguments
+    for(var count = 0; count < keys.length; count++) {
+      const key = keys[count]
+
+      // create query for provided fields in body
+      var query = {}
+      query[key] = {$in: [req.body[key][0], req.body[key][1]]}
+
+      // query database
+      const players = await Player.find(query)
+
+      if(players && players.length == 2) {
+        const comparison = getDifference(players)
+        res.send({players, comparison})
+      }
+    }
+
+    throw new Error()
+  } catch(error) {
+    res.status(404).send(error)
   }
 })
 
